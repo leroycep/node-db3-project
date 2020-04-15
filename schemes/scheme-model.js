a26f3fa0c8c7fc9cc492f3497231f4911855bc02
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-04-15 10:30:13 -0600

    Add `update(scheme, id)` database helper

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -3,6 +3,14 @@ const knexfile = require("../knexfile.js");
 
 const database = knex(knexfile.development);
 
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+};
+
 function find() {
   return database("schemes").select();
 }
@@ -24,12 +32,15 @@ function add(scheme) {
     .then((new_ids) => {
       const id = new_ids[0];
       return database("schemes").select().where({ id }).first();
-    }).catch(err => console.log(err));
+    });
 }
 
-module.exports = {
-  find,
-  findById,
-  findSteps,
-  add,
-};
+function update(changes, id) {
+  return database("schemes")
+    .where({ id })
+    .update(changes)
+    .then((_num_updated) => {
+      return database("schemes").select().where({ id }).first();
+    });
+}
+
