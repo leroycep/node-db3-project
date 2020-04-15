aba3c1b47584a4a024fade51c812b74946f274e7
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-04-15 10:13:57 -0600

    Add `findSteps(scheme_id)` database helper

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -11,7 +11,15 @@ function findById(scheme_id) {
   return database("schemes").select().where({ id: scheme_id }).first();
 }
 
+function findSteps(scheme_id) {
+  return database("steps")
+    .select("steps.id as id", "scheme_name", "step_number", "instructions")
+    .where({ scheme_id })
+    .join("schemes", { "steps.scheme_id": "schemes.id" });
+}
+
 module.exports = {
   find,
   findById,
+  findSteps,
 };
